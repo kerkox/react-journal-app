1bc09ce32178b782601c93dbb60ad90dc4c05ce1
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-19 11:30:04 -0500

    styles in auth

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import { Redirect, Route, Switch } from 'react-router-dom'
-import { LoginScreen } from '../componentes/auth/LoginScreen'
-import { RegisterScreen } from '../componentes/auth/RegisterScreen'
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { LoginScreen } from "../componentes/auth/LoginScreen";
+import { RegisterScreen } from "../componentes/auth/RegisterScreen";
 
 export const AuthRouter = () => {
-  
   return (
-    <Switch>
-      <Route exact path="/auth/login" component={LoginScreen} />
-      <Route exact path="/auth/register" component={RegisterScreen} />
-      <Route>
-        <Redirect to="/auth/login" />
-      </Route>
-    </Switch>
+    <div className="auth__main">
+      <div className="auth__box-container">
+        <Switch>
+          <Route exact path="/auth/login" component={LoginScreen} />
+          <Route exact path="/auth/register" component={RegisterScreen} />
+          <Route>
+            <Redirect to="/auth/login" />
+          </Route>
+        </Switch>
+      </div>
+    </div>
   );
-}
+};
