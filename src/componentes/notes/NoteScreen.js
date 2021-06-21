848bcf775da4016bd51061bf7b3954bf28e1d5a7
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-21 15:41:25 -0500

    Avances de notes

--- src/componentes/notes/NoteScreen.js
+++ src/componentes/notes/NoteScreen.js
@@ -1,10 +1,33 @@
-import React from 'react'
-import { NotesAppBar } from './NotesAppBar'
+import React from "react";
+import { NotesAppBar } from "./NotesAppBar";
 
 export const NoteScreen = () => {
   return (
     <div className="notes__main-content">
       <NotesAppBar />
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__title-input"
+          autoComplete="off"
+        />
+        <textarea
+          placeholder="What happended today?"
+          className="notes__textarea"
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+        ></textarea>
+
+        <div className="notes__image">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C561BAQGEbvT3SFyR9Q/company-background_10000/0/1582050035728?e=2159024400&v=beta&t=xwPLRsVBBNXQQS3HN3q7hsYXmt6JxJsH6lpnbh9Y1ko"
+            alt="imagen"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
