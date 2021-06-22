8d5046ffc8cccb69cb6a67de79814b57a44c435a
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-21 21:40:46 -0500

    rename de la carptea de compoentes e inclusidon de dispatch

---
+++ src/components/auth/LoginScreen.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { useDispatch } from 'react-redux'
+import { Link } from "react-router-dom";
+import { login } from "../../actions/auth";
+import { useForm } from "../../hooks/useForm";
+
+export const LoginScreen = () => {
+
+  const dispatch = useDispatch()
+
+  const [formValues, handleInputChange] = useForm({
+    email: "prueba@prueba.com",
+    password: "123456",
+  });
+
+  const { email, password } = formValues;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(12345,'Paul'))
+  }
+
+  return (
+    <>
+      <h3 className="auth__title mb-5">Login</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="auth__input"
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={email}
+          onChange={handleInputChange}
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={password}
+          onChange={handleInputChange}
+        />
+        <button className="btn btn-primary btn-block" type="submit">
+          Login
+        </button>
+        <div className="auth__social-networks">
+          <p>Login with social networks</p>
+          <div className="google-btn">
+            <div className="google-icon-wrapper">
+              <img
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="google button"
+              />
+            </div>
+            <p className="btn-text">
+              <b>Sign in with google</b>
+            </p>
+          </div>
+        </div>
+        <Link to="/auth/register" className="link">
+          Create a new account
+        </Link>
+      </form>
+    </>
+  );
+};
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
-import { JournalScreen } from '../componentes/journal/JournalScreen'
+import { JournalScreen } from '../components/journal/JournalScreen'
 import { AuthRouter } from './AuthRouter'
 
 export const AppRouter = () => {
