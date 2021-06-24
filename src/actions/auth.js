d776de30483f9e2703912c07e3a9fd2d16e1c744
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-24 12:27:12 -0500

    Demo de un dispathc asincrono

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,9 +1,17 @@
 import { types } from "../types/types";
 
+export const startLoginEmailPassword = (email, password) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(login(email, password));
+    }, 3500);
+  };
+};
+
 export const login = (uid, displayName) => ({
   type: types.login,
   payload: {
     uid,
     displayName,
-  }
+  },
 });
