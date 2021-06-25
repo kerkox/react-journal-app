9a2da85e05a62ef759ca47af6aa86b91020f39cb
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-24 21:45:47 -0500

    validar si el usuario esta autenticado

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useEffect } from 'react'
 import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
 import { JournalScreen } from '../components/journal/JournalScreen'
 import { AuthRouter } from './AuthRouter'
+import { firebase } from '../firebase/firebase-config'
+import { useDispatch } from 'react-redux'
+import { login } from '../actions/auth'
 
 export const AppRouter = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName))
+      }
+    })
+  }, [dispatch])
   
   return (
     <Router>
