e3a3c76facc0b7719a45ac1a3e956303ce3bccb8
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-24 21:09:43 -0500

    Validacion con el formulario y redux

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,26 +1,59 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { useForm } from '../../hooks/useForm';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+import validator from "validator";
+import { types } from "../../types/types";
+import { useDispatch } from "react-redux";
+import { setError } from "../../actions/ui";
+import { useSelector } from "react-redux";
 
 export const RegisterScreen = () => {
+  const dispatch = useDispatch();
+  const { msgError } = useSelector((state) => state.ui);
 
-  const [ formValues, handleInputChange ] = useForm({
-    name:'Paul',
-    email:'paul@paul.com',
-    password:'123456',
-    password_confirm:'123456'
+  const [formValues, handleInputChange] = useForm({
+    name: "Paul",
+    email: "paul@paul.com",
+    password: "123456",
+    password_confirm: "123456",
   });
 
-  const {name,email,password,password_confirm} = formValues
+  const { name, email, password, password_confirm } = formValues;
 
   const handleRegister = (e) => {
     e.preventDefault();
-    console.log(formValues)
-  } 
+    if (isFormValid()) {
+      console.log("Formulario correcto");
+    }
+  };
+
+  const isFormValid = () => {
+    if (name.trim().length === 0) {
+      dispatch(setError("Name is required"));
+      return false;
+    } else if (!validator.isEmail(email)) {
+      dispatch(setError("Email is not valid"));
+      return false;
+    } else if (password !== password_confirm || password.length < 6) {
+      dispatch(
+        setError(
+          "Password should be at least 6 characters and match each other"
+        )
+      );
+      return false;
+    }
+
+    dispatch({
+      type: types.uiRemoveError,
+    });
+    return true;
+  };
+
   return (
     <>
       <h3 className="auth__title mb-5">Register</h3>
       <form onSubmit={handleRegister}>
+        {msgError && <div className="auth__alert-error">{msgError}</div>}
         <input
           className="auth__input"
           onChange={handleInputChange}
@@ -65,4 +98,4 @@ export const RegisterScreen = () => {
       </form>
     </>
   );
-}
+};
