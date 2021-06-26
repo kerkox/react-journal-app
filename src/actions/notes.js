099df137165a39e29ab8e2beb11db999c090b234
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-26 17:20:10 -0500

    carga de notas desde firestore

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -23,3 +23,8 @@ export const activeNote = (id, note) => ({
     id, ...note
   }
 })
+
+export const setNotes = (notes) => ({
+  type:types.notesLoad,
+  payload: notes
+})
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -14,6 +14,8 @@ import { login } from "../actions/auth";
 import { useState } from "react";
 import { PublicRoute } from "./PublicRoute";
 import { PrivateRoute } from "./PrivateRoute";
+import { loadNotes } from "../helpers/loadNotes";
+import { setNotes } from "../actions/notes";
 
 export const AppRouter = () => {
   const dispatch = useDispatch();
@@ -22,10 +24,13 @@ export const AppRouter = () => {
   const [{ isLoggedIn }, setIsLoggedIn] = useState({ isLoggedIn: false });
 
   useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged(async (user) => {
       if (user?.uid) {
         dispatch(login(user.uid, user.displayName));
         setIsLoggedIn({ isLoggedIn: true });
+
+        const notes = await loadNotes(user.uid)
+        dispatch(setNotes(notes))
       } else {
         setIsLoggedIn({ isLoggedIn: false });
       }
