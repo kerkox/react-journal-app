68f2e4089d264477be81dafb74dd499a142f6c9e
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-26 16:45:10 -0500

    Nuevo reducer de notas

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from "redux-thunk";
 import { authReducer } from '../reducers/authReducer';
+import { notesReducer } from '../reducers/notesReducer';
 import { uiReducer } from '../reducers/uiReducer';
 
 const composeEnhancers =
@@ -12,7 +13,8 @@ const composeEnhancers =
 
 const reducers = combineReducers({
   auth: authReducer,
-  ui: uiReducer
+  ui: uiReducer,
+  notes:  notesReducer
 })
 
 
