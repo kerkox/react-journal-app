41a9e552d984c84de9e7c9c04e96c091d12700a2
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-26 03:43:16 -0500

    Ajustes para validar el login

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,18 +6,32 @@ import { AuthRouter } from './AuthRouter'
 import { firebase } from '../firebase/firebase-config'
 import { useDispatch } from 'react-redux'
 import { login } from '../actions/auth'
+import { useState } from 'react'
 
 export const AppRouter = () => {
 
   const dispatch = useDispatch();
 
+  const [{checking}, setChecking] = useState({checking:true});
+  const [{ isLoggedIn }, setIsLoggedIn] = useState({isLoggedIn:false});
+
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       if (user?.uid) {
-        dispatch(login(user.uid, user.displayName))
+        dispatch(login(user.uid, user.displayName));
+        setIsLoggedIn({isLoggedIn:true})
+      } else {
+        setIsLoggedIn({isLoggedIn:false})
       }
+      setChecking({checking:false})
     })
-  }, [dispatch])
+  }, [dispatch, setChecking])
+
+  if ( checking ) {
+    return (
+      <h1>Espere...</h1>
+    )
+  }
   
   return (
     <Router>
