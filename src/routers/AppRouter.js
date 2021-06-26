43e9fdf2e6e7dd0a8c25968def922b774dabeda4
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-26 17:23:58 -0500

    Carga de las notas desde el action

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { useEffect } from "react";
 import {
-  Route,
   BrowserRouter as Router,
-  Switch,
-  Redirect,
+  Switch
 } from "react-router-dom";
 import { JournalScreen } from "../components/journal/JournalScreen";
 import { AuthRouter } from "./AuthRouter";
@@ -14,8 +12,7 @@ import { login } from "../actions/auth";
 import { useState } from "react";
 import { PublicRoute } from "./PublicRoute";
 import { PrivateRoute } from "./PrivateRoute";
-import { loadNotes } from "../helpers/loadNotes";
-import { setNotes } from "../actions/notes";
+import { startLoadingNotes } from "../actions/notes";
 
 export const AppRouter = () => {
   const dispatch = useDispatch();
@@ -29,8 +26,8 @@ export const AppRouter = () => {
         dispatch(login(user.uid, user.displayName));
         setIsLoggedIn({ isLoggedIn: true });
 
-        const notes = await loadNotes(user.uid)
-        dispatch(setNotes(notes))
+        dispatch(startLoadingNotes(user.uid))
+        
       } else {
         setIsLoggedIn({ isLoggedIn: false });
       }
