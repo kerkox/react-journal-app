51f09fefbd2de75ea0122b2b16b10b47197ab529
Paul Cortes Renteria <pcortesr@psl.com.co>
2021-06-26 17:03:25 -0500

    insercion de notas en Firebase Store

--- src/types/types.js
+++ src/types/types.js
@@ -5,6 +5,14 @@ export const types = {
   uiSetError: '[UI] set error',
   uiRemoveError: '[UI] remove error',
   uiStartLoading: '[UI] Start loading',
-  uiFinishLoading: '[UI] Finish loading'
+  uiFinishLoading: '[UI] Finish loading',
+
+  notesAddNew: '[Notes] New note',
+  notesActive: '[Notes] Set active note',
+  notesLoad: '[Notes] Load notes',
+  notesUpdated: '[Notes] Update note',
+  notesFileUrl: '[Notes] Update image url',
+  notesDelete: '[Notes] Delete note',
+  notesLogoutCleaning: '[Notes] Logout Cleaning',
 
 }
